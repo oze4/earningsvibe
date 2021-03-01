ae9789bf27a63b49719d06500b4e80d7baed3581
Matt Oestreich <matthewpoestreich@gmail.com>
2021-02-28 21:54:33 -0600

    init

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ const helmet = require('helmet');
 const path = require('path');
 const FMPCloud = require('./fmpcloud');
 
+const fmpcloud = new FMPCloud();
 const app = express();
 const appPort = 8081;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(helmet());
-
 app.use(express.static(path.join(__dirname, '../build')));
 app.use('/static', express.static(path.join(__dirname, '../build/static')));
 
@@ -18,17 +17,27 @@ app.get('/', (_req, res) => {
   res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
-app.get('/profile/:symbol', async (req, res) => {
+app.get('/api/profile/:symbol', async (req, res) => {
   try {
-    const fmpcloud = new FMPCloud();
     const data = await fmpcloud.CompanyProfile(req.params.symbol);
     res.status(200).send(data);
-  } catch (e) {
-    res.status(500).send(e);
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
 
-app.get('/earnings/:symbol', async (req, res) => {});
+app.get('/api/earnings/:symbol/:yearsAgo', async (req, res) => {
+  try {
+    const { symbol, yearsAgo } = req.params;
+    if (!yearsAgo) {
+      yearsAgo = 1;
+    }
+    const data = await fmpcloud.HistoricalEarnings(symbol, yearsAgo);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.get('*', (_req, res) => {
   res.status(404).send({ response: null, status: 404 });
