d3c62b4f392e39bf712560a8d3ac26745e89c341
oze4 <matthewpoestreich@gmail.com>
2021-04-01 00:08:08 -0500

    Update index.js

--- frontend/src/components/BodyContainer/index.js
+++ frontend/src/components/BodyContainer/index.js
@@ -6,26 +6,3 @@ export default function BodyContainer(props) {
 
   return <Container {...rest}>{children}</Container>;
 }
-
-function MyComponent() {
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth
-  });
-  
-  React.useEffect(() => {
-    function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth
-      });
-    }
-    window.addEventListener('resize', handleResize);
-  });
-
-  return (
-    <div>
-      Rendered at {dimensions.width} x {dimensions.height}
-    </div>
-  );
-}
