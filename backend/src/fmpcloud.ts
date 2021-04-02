1c19d84da8734d16d91a75537ec2cfdf5fc720cd
oze4 <matthewpoestreich@gmail.com>
2021-04-01 19:58:07 -0500

    Update fmpcloud.ts

--- backend/src/fmpcloud.ts
+++ backend/src/fmpcloud.ts
@@ -162,8 +162,6 @@ export default class FMPCloud {
       return earnings.map((earning) => {
         let vibe = { earning, stock: [] as Stock[] };
         stockDatas.forEach((stockData) => {
-          stockData.map((sd) => sd.date).join(', ');
-          stockData;
           const stockDataDate = new Date(stockData[0].date);
           console.log(
             `${stockDataDate.valueOf()} >= ${earning.daysBefore.valueOf()} && ${stockDataDate.valueOf()} <= ${earning.daysAfter.valueOf()}`,
